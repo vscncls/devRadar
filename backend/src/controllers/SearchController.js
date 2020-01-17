641ee9abc52dbc69a6b4d50a230cb1d4bd2076a0
Lucas <lucaslou4@gmail.com>
2020-01-16 23:11:16 -0300

    feat: if no tech is given, find all

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -7,17 +7,20 @@ routes.get("/", async (req, res) => {
   const { latitude, longitude, techs } = req.query;
   const techsArray = techs.split(",").map(tech => tech.trim());
 
+  const searchTechs = {};
+  if (techs) {
+    searchTechs.techStack = { $in: techsArray };
+  }
+
   const devs = await Dev.find({
-    techStack: {
-      $in: techsArray
-    },
+    ...searchTechs,
     location: {
       $near: {
         $geometry: {
           type: "Point",
           coordinates: [longitude, latitude]
         },
-        $maxDistance: 10000
+        $maxDistance: 100000
       }
     }
   });
